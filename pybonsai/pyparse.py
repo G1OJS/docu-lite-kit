0e79d2763808b8525ed693ff17fd387b8829f8fc
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-07-31 09:16:57 +0100

    Rewrite

--- pybonsai/pyparse.py
+++ pybonsai/pyparse.py
@@ -1,38 +1,20 @@
+class files:
+    def __init__(self, filepath):
+        self.lines = ""
+        self.lines = self._read(filepath)
+        
+    def _read(self, filepath):
+        with open(filepath) as f:
+            return f.readlines()
+
 class pbNode:
-    def __init__(self, first_line_number, doclines):
-        self.indent = self._line_indent(doclines[first_line_number])
-        self.signature = doclines[first_line_number].split()
+    def __init__(self, first_line_number, line, object_pattern):
         self.first_line_number = first_line_number
-        self.content_start_line_number = first_line_number
-        self.last_line_number = len(doclines) - 1
-        self.docstring = ""
+        self.line_indent = self._line_indent(line)
+        self.object_pattern = object_pattern
+        self.last_line_number = 0
         self.twigs = []
-
-        docstring_lines = None
-        line_no = first_line_number - 1
-        while (line_no  < self.last_line_number):
-            line_no += 1
-            if(docstring_lines is None):
-                docstring_delimeter = self._has_docstring_delimeter(doclines[line_no])
-                if(docstring_delimeter):        
-                    docstring_lines = []
-                    doclines[line_no] = doclines[line_no].replace(docstring_delimeter,'',1)
-            if(docstring_lines is not None):
-                docstring_lines.append(doclines[line_no])
-                if(docstring_delimeter in doclines[line_no]):
-                    self.docstring = [line.rstrip().replace(docstring_delimeter, '').replace('\n','') for line in docstring_lines]
-                    self.docstring = [line for cnt,line in enumerate(self.docstring) if line.strip() or (cnt !=0 and cnt != len(self.docstring)-1)]
-                    docstring_lines = None
-                    self.content_start_line_number = line_no + 1
-            if(line_no < self.last_line_number):
-                new_block = doclines[line_no+1].rstrip().endswith(':')
-                dedent = ( (docstring_lines is None) and self._line_indent(doclines[line_no+1]) <= self.indent )
-                if(new_block or dedent):
-                    self.last_line_number = line_no+1
-                    break
-        self.content = [line.rstrip().replace('\n','') for line in doclines[self.content_start_line_number:self.last_line_number]]
-
-            
+        
     def _line_indent(self, line):
         line = line.rstrip()
         if(len(line.strip()) == 0):
@@ -40,29 +22,96 @@ class pbNode:
         else:
             return len(line) - len(line.lstrip())
 
+class pbList:
+    """
+        Uses the docu-lite method of reading all lines sequentially to generate a flat list of objects where
+        docstring and non-docstring content are counted as objects alongside object_pattern objects. The purpose of
+        doing this is to ensure that nothing is missed, and this list is organised into a tree later
+    """
+    def __init__(self, doc_objects, doclines, object_patterns = ['def', 'class']):
+        self.object_patterns = object_patterns
+        self.objects = doc_objects
+
+        in_docstring = False
+        for line_no, line in enumerate(doclines):
+            object_pattern_found = self._has_object_pattern(line)
+            if (object_pattern_found):
+                self.objects.append(pbNode(line_no, line, object_pattern_found))
+            docstring_closure = self._has_docstring_delimeter(line)
+            if(docstring_closure):
+                if(not in_docstring):
+                    self.objects.append(pbNode(line_no, line, 'docstring'))
+                    in_docstring = True
+                else:
+                    self.objects[-1].last_line_number = line_no
+                    in_docstring = False
+
+    def _has_object_pattern(self, line):
+        l = line.strip()
+        for pat in self.object_patterns:
+            if (l.startswith(pat) and line.endswith(':\n')):
+                return line.split()[0]
+        return False
+
     def _has_docstring_delimeter(self,line):
         for delim in ['"""',"'''"]:
-            if(delim in line):
+            if(line.strip().startswith(delim) or line.strip().endswith(delim)):
                 return delim
         return False
 
+
+class test:
+    # where to set last line number - naturally when making tree?
+    def __init__(self, pyfile):
+        doc_objects = []
+        pylines = files(pyfile).lines
+        doc_objects = pbList(doc_objects, pylines, ['def','class']).objects
+        scopelist = []
+        for pbNode in doc_objects:
+            scopelist.append((pbNode.first_line_number, pbNode.last_line_number, pbNode.object_pattern))            
+        for lno, line in enumerate(pylines):
+            dline = f"{lno:4g}: {line.strip()[0:20]:<20}| "
+            for s in scopelist:
+                fl,ll,pat = s
+                if(lno==fl):
+                    dline += pat + " ->" + str(ll)
+            print(dline) 
+            indent_str = ">" * pbNode.line_indent + "|"
+        
+
+class pbTree:
+    """
+        Uses pbList to get flat list if one doesn't already exist, and packages this into a tree structure based
+        on the indent level of each object
+    """
+    def __init__(self, doc_objects):
+        self.doc_objects = doc_objects
+            
+        
+
+
+class pbBrowser:
+    """
+        Generates HTML for expandable node list representing the document tree
+    """
     def _format_docstring(self, docstring_lines):
         lines = [line.rstrip().replace(docstring_delimeter, '') for line in docstring_lines]
 
-class pbTree:
 
-    def __init__(self,filepath):
-        with open(filepath) as f:
-            doclines = f.readlines()
-        self.pbRoot = pbNode(0, doclines)
-        self.pbNodes = [self.pbRoot]
-        ancestry = [self.pbRoot]
-        while (self.pbNodes[-1].last_line_number < len(doclines) -1):
-            self.pbNodes.append(pbNode(self.pbNodes[-1].first_line_number + 1, doclines))
-            while ancestry and self.pbNodes[-1].indent < ancestry[-1].indent:
-                ancestry.pop()
-            ancestry[-1].twigs.append(self.pbNodes[-1])
-            ancestry.append(self.pbNodes[-1])
-            
+class pbUserguide:
+    """
+        Generates HTML for user guide
+    """
+    def dummy():
+        a=1
 
 
+class pbListAPI:
+    """
+        Generates text list representing API functions only
+    """
+    def dummy2():
+        a=1
+
+
+t = test("./pyparse.py")
