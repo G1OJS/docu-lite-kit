7440188327eec4e28b5a0b152406de32b3b352b4
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-07-31 22:05:53 +0100

    Removed trees
    HTML nearly working from object list. Need to look at indent level of docstring vs content and how it should be displayed.

--- pybonsai/pyparse.py
+++ pybonsai/pyparse.py
@@ -1,3 +1,7 @@
+"""
+    file-level docstring
+"""
+
 class files:
     def __init__(self, filepath):
         self.lines = ""
@@ -7,48 +11,50 @@ class files:
         with open(filepath) as f:
             return f.readlines()
 
-class pbNode:
+class pbBlock:
     def __init__(self, first_line_number, doclines, pattern):
         self.first_line_number = first_line_number
-        self.line_indent = 10000
+        self.last_line_number = len(doclines) - 1
+        self.indent_spaces = 10000
+        self.indent_level = 0
         self.pattern = pattern
         self.signature = ""
-        self.last_line_number = 0
-        self.twigs = []
+        self.icon = ""
 
         line = doclines[self.first_line_number].rstrip()
         if(len(line.strip()) != 0):
-            self.line_indent = len(line) - len(line.lstrip())
+            self.indent_spaces = len(line) - len(line.lstrip())
 
-class pbTree:
+    def get_content(self, doclines):
+        return doclines[self.first_line_number:self.last_line_number+1]
+        
+class pbBlocks:
     """
         Uses the docu-lite method of reading all lines sequentially to generate a flat list of blocks where
-        docstring and non-docstring content are counted as blocks alongside pattern blocks. The purpose of
-        doing this is to ensure that nothing is missed, and this list is organised into a tree later
+        docstring and non-docstring content are counted as blocks alongside pattern blocks.
     """
     def __init__(self, doclines, patterns = ['def', 'class']):
         self.patterns = patterns
-        self.block_list = []
-        self.root = pbNode(0, "root()", "root")
-        self.block_tree =[self.root]
+        self.blocks = []
 
         # get all blocks in a flat list
         in_docstring = False
         for line_no, line in enumerate(doclines):
             pattern_found = self._has_pattern(line)
             if(pattern_found):
-                self.block_list.append(pbNode(line_no, doclines, pattern_found))
+                self.blocks.append(pbBlock(line_no, doclines, pattern_found))
             else:
                 docstring_closure = self._has_docstring_delimeter(line)
                 if(docstring_closure):
                     if(not in_docstring):
-                        self.block_list.append(pbNode(line_no, doclines, 'docstring'))
+                        self.blocks.append(pbBlock(line_no, doclines, 'docstring'))
                         in_docstring = not self._has_docstring_delimeter(line.replace(docstring_closure,'',1))
                     else:
                         in_docstring = False
+                        self.blocks[-1].last_line_number = line_no
      
         # set object signatures
-        for block in self.block_list[1:]:
+        for block in self.blocks[1:]:
             if(block.pattern == 'docstring'):
                 continue
             first_line = doclines[block.first_line_number]
@@ -60,16 +66,24 @@ class pbTree:
                     lno += 1
                 block.signature = sigtext[:sigtext.find(')')+1].replace(block.pattern,'')
             else:
-                block.signature = first_line.split()[1:]
+                block.signature = "".join(first_line.split()[1:])
 
-        # make the document tree
-        ancestry = [self.root]
-        for pbN in self.block_list:
-            # Find parent by comparing indentation
-            while len(ancestry)>1 and pbN.line_indent < ancestry[-1].line_indent:
-                ancestry.pop()
-            ancestry[-1].twigs.append(pbN)
-            ancestry.append(pbN)
+        # tell each object what its indent level is within the document
+        indents =[0]
+        for pbB in self.blocks:
+            if(pbB.indent_spaces > indents[-1]):
+                indents.append(pbB.indent_spaces)
+            pbB.indent_level = indents.index(pbB.indent_spaces)
+
+        # find and set last line numbers
+        for block_number, block in enumerate(self.blocks[1:]):
+            bn = block_number
+            while(bn>0):
+                if(block.indent_level <= self.blocks[bn].indent_level):
+                    potential_last_line_number = block.first_line_number - 1
+                    if(potential_last_line_number < self.blocks[bn].last_line_number):
+                        self.blocks[bn].last_line_number = potential_last_line_number
+                bn -= 1
 
     def _has_pattern(self, line):
         l = line.strip()
@@ -89,48 +103,61 @@ class pbTree:
 class test:
     def __init__(self, pyfile):
         pylines = files(pyfile).lines
-        graphs = pbTree(pylines, ['def', 'class'])
-        doc_blocks = graphs.block_list
-
-        list_lines = self._pbList_lines(doc_blocks)
-        tree_lines = self._pbTree_lines(graphs.root)
-
-        print(f"{'Line':<5} | {'Python source':<25} | {'Block list':<25} | {'Block tree':<25}")
+        doc_blocks = pbBlocks(pylines, ['def', 'class']).blocks
+        first_line_info = {pb.first_line_number: f"{'>' * pb.indent_level} {pb.first_line_number + 1}-{pb.last_line_number + 1}: {pb.pattern} {pb.signature}" for pb in doc_blocks}
+        print(f"{'Line':<5} | {'Python source':<25} | {'Block list':<25}")
         print("-" * 89)
         for lno, line in enumerate(pylines):
             src = line.rstrip()
-            block_info = list_lines[lno] if lno in list_lines else f"src: {src}"
-            tree_info = tree_lines[lno] if lno in tree_lines else f"src: {src}"
-            print(f"{lno+1:<5} | {src[:25]:<25} | {block_info[:25]:<25} | {tree_info[:25]:<25}")
+            block_info = first_line_info[lno] if lno in first_line_info else f"src: {src}"
+            print(f"{lno+1:<5} | {src[:25]:<25} | {block_info:<25}")
 
-    def _pbList_lines(self, doc_blocks):
-        output_lines = {}
-        for pbNode in doc_blocks:
-            output_lines[pbNode.first_line_number] = self._fString(pbNode)
-        return output_lines
-
-    def _pbTree_lines(self, pbNode, output_lines=None):
-        output_lines = {} if output_lines is None else output_lines
-        if pbNode.pattern:
-            output_lines[pbNode.first_line_number] =  self._fString(pbNode)
-        for twig in pbNode.twigs:
-            self._pbTree_lines(twig, output_lines)
-
-        return output_lines
-
-    def _fString(self, pbNode):
-        indent_str = ">" * pbNode.line_indent
-        return f"{indent_str} {pbNode.pattern} {pbNode.signature}"
-
-
+    def _fString(self, pbBlock):
+        indent_str = ">" * pbBlock.indent_spaces
+        return f"{indent_str} {pbBlock.pattern} {pbBlock.signature}"
 
 class pbBrowser:
     """
         Generates HTML for expandable node list representing the document tree
     """
-
+    def __init__(self, pyfile):
+        self.pylines = files(pyfile).lines
+        self.blocks = pbBlocks(self.pylines, ['def', 'class']).blocks
+        style_sheet_file = "docu-lite-style.css"
+        self.html =  f"<!DOCTYPE html><html lang='en'>\n<head>\n<title></title>"
+        self.html += f"<link rel='stylesheet' href='./{style_sheet_file}' />"
+        prev_indent = 0
+        for pbB in self.blocks:
+            self.html += self._signature_html(pbB.pattern, pbB.signature, True)
+            self.html += self._content_html(pbB.pattern, pbB.get_content(self.pylines))
+            self.html += self._close_details(pbB.indent_level-prev_indent+1 )
+            prev_indent = pbB.indent_level
+        soft_string = ''
+        self.html += f"\n<br><br><span style = 'font-size:0.8em;color:#666;border-top:1px solid #ddd; "
+        self.html += f"font-style:italic'>Made with {soft_string}</span></body>\n"
 
     
+    def _signature_html(self, obj_type, obj_signature, open_details = True):
+        htm = "<details><summary>" if open_details else "<div>"
+        htm += f"<span class ='{obj_type} {'signature'}'>{obj_signature}</span>"
+        htm += "</summary>" if open_details else "</div>"
+        return htm + "\n"
+
+    def _content_html(self, object_type, content_lines):
+        import html
+        htm = f"<pre class ='{object_type} content'>"
+        for i, line in enumerate(content_lines):
+            if(i==0 and len(line.strip()) ==0):
+                continue
+            htm += f"{html.escape(line)}"
+        htm += "</pre>\n"
+        return htm
+
+    def _close_details(self, n_times):
+        if(n_times <1):
+            return ""
+        return "</details>\n" * n_times
+
     def _format_docstring(self, docstring_lines):
         lines = [line.rstrip().replace(docstring_delimeter, '') for line in docstring_lines]
 
@@ -151,8 +178,11 @@ class pbListAPI:
         a=1
 
 def main():
-    t = test("./pyparse.py")
-
-
+   # t = test("./pyparse.py")
+    output_html_file = "test.html"
+    htm = pbBrowser(r"C:\Users\drala\Documents\Projects\GitHub\NECBOL\necbol\modeller.py").html
+    with open(output_html_file, "w", encoding="utf-8") as f:
+        f.write(htm)
+            
 if __name__ == "__main__":
     main()
