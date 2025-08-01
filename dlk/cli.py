daac4fa040b581b417555b2d8e4a55d0d4675957
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-08-01 23:35:34 +0100

    fix cli entry point issue

--- dlk/cli.py
+++ dlk/cli.py
@@ -2,16 +2,17 @@
 import argparse
 from .dlkParse import *
 
-parser = argparse.ArgumentParser()
-parser.add_argument("infile")
-parser.add_argument("--out", help = "Output file (json format)")
-parser.add_argument("--noprint", action = 'store_true', help = "Don't print output to console")
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infile")
+    parser.add_argument("--out", help = "Output file (json format)")
+    parser.add_argument("--noprint", action = 'store_true', help = "Don't print output to console")
+    args = parser.parse_args()
 
-if(args.infile):
-    dlk = dlkIO(args.infile)
-    if(not args.noprint):
-        dlk.dlkPrint()
-    if(args.out):
-        dlk.dlkDumpJSON(args.out) 
+    if(args.infile):
+        dlk = dlkIO(args.infile)
+        if(not args.noprint):
+            dlk.dlkPrint()
+        if(args.out):
+            dlk.dlkDumpJSON(args.out) 
 
